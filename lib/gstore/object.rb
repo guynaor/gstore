8f3a2ba01a598d88a8185b1b13a79cfb119586e8
Guy Naor <guy.naor@gmail.com>
2010-07-27 13:50:23 -0700

    * The object name is now escaped so it doesn't break HTTP * Add the initial slash only of none was provided

--- lib/gstore/object.rb
+++ lib/gstore/object.rb
@@ -1,12 +1,12 @@
 module GStore
   class Client
     def put_object(bucket, filename, options={})
-      put(bucket, "/#{filename}", options)
+      put(bucket, add_slash_if_needed(filename), options)
     end
     
     def get_object(bucket, filename, options={})
       outfile = options.delete(:outfile)
-      res = get(bucket, "/#{filename}", options)
+      res = get(bucket, add_slash_if_needed(filename), options)
       if outfile
         File.open(outfile, 'w') {|f| f.write(res) }
       else
@@ -15,11 +15,17 @@ module GStore
     end
     
     def delete_object(bucket, filename, options={})
-      delete(bucket, "/#{filename}", options)
+      delete(bucket, add_slash_if_needed(filename), options)
     end
     
     def get_object_metadata(bucket, filename, options={})
-      head(bucket, "/#{filename}", options)
+      head(bucket, add_slash_if_needed(filename), options)
+    end
+
+    # Add a starting / if one was not provided
+    # And encode ? as it breaks the sending
+    def add_slash_if_needed(filename)
+      CGI::escape(filename[0..0] == '/' ? filename : "/#{filename}").gsub('%2F', '/')
     end
   end
 end
